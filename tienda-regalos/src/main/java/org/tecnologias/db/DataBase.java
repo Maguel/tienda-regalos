c125ba4cc97c6447ff55906192bb79d721b42e23
Maguel <miguelangel.1999.10@gmail.com>
2023-12-01 12:50:12 -0600

    Add password validators feature

--- tienda-regalos/src/main/java/org/tecnologias/db/DataBase.java
+++ tienda-regalos/src/main/java/org/tecnologias/db/DataBase.java
@@ -3,11 +3,21 @@ package org.tecnologias.db;
 import java.util.HashMap;
 
 public class DataBase {
+    private static final String ADMIN_PASS = "admin";
+    private static final String EMPLEADO_PASS = "empleado";
     private final HashMap<Integer, Peluche> inventario = new HashMap<>();
+
+    public static boolean validarAdmin(String pass) {
+        return ADMIN_PASS.equals(pass);
+    }
+    public static boolean validarEmpleado(String pass) {
+        return EMPLEADO_PASS.equals(pass);
+    }
 
     public boolean agregarProducto(Peluche peluche) {
         return inventario.put(peluche.getCodigo(), peluche) == null;
     }
+
     public boolean modificarCantidad(Peluche peluche, Integer cantidad) {
         if (inventario.containsKey(peluche.getCodigo())) {
             inventario.get(peluche.getCodigo()).setCantidad(cantidad);
@@ -15,9 +25,11 @@ public class DataBase {
         }
         return false;
     }
+
     public boolean eliminarProducto(Peluche peluche) {
         return inventario.remove(peluche.getCodigo()) == null;
     }
+
     private DataBase() {
     }
 
