8300dc7f56431ca124469f0d9d02547feedda3ba
Eber_Lab <eberto481@gmail.com>
2023-12-01 18:43:50 -0600

    Update Inventario.java

--- tienda-regalos/src/main/java/org/tecnologias/admin/Inventario.java
+++ tienda-regalos/src/main/java/org/tecnologias/admin/Inventario.java
@@ -6,6 +6,23 @@ import org.tecnologias.db.Peluche;
 public class Inventario {
     private DataBase base = DataBase.getInstance();
 
-    public boolean agregarProducto(Peluche peluche){return  false;
+    public boolean agregarProducto(Peluche peluche){
+        return base.agregarProducto(peluche);
+    }
+
+    public boolean eliminarProducto(Peluche peluche){
+        return base.eliminarProducto(peluche);
+    }
+
+    public boolean modificarCantidad(Peluche peluche, Integer cantidad){
+        return base.modificarCantidad(peluche,cantidad);
+    }
+
+    public void getInventario(){
+        base.getInventario();
+    }
+
+    public Peluche getProducto(int id){
+        return base.getProducto(id);
     }
 }
