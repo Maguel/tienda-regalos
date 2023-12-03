f85babeac7abe04100c7db197f3215fbd36ed774
Eber_Casa <vazquezeber88@gmail.com>
2023-12-02 18:49:17 -0600

    Add ConcreteFatories

---
+++ tienda-regalos/src/main/java/org/tecnologias/admin/CreadorFacturaMattel.java
@@ -0,0 +1,21 @@
+package org.tecnologias.admin;
+
+import org.tecnologias.db.Peluche;
+import org.tecnologias.db.Provedor;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CreadorFacturaMattel extends CreadorFactura{
+
+
+    protected Factura crearFactura(Date fecha, float monto, Provedor provedor, ArrayList<Peluche> articulos) {
+        return new  FacturaMattel(fecha, monto, provedor, articulos);
+    }
+
+    protected void agregarFacturas(Factura factura){
+        facturas.add(factura);
+    }
+
+
+}
---
+++ tienda-regalos/src/main/java/org/tecnologias/admin/CreadorFacturaMuñelocos.java
@@ -0,0 +1,20 @@
+package org.tecnologias.admin;
+
+import org.tecnologias.db.Peluche;
+import org.tecnologias.db.Provedor;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CreadorFacturaMuñelocos extends CreadorFactura{
+
+    protected Factura crearFactura(Date fecha, float monto, Provedor provedor, ArrayList<Peluche> articulos) {
+        return new FacturaMuñelocos(fecha,monto,provedor,articulos);
+    }
+
+
+    protected void agregarFacturas(Factura factura) {
+        facturas.add(factura);
+    }
+
+}
