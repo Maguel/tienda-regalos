ce9d6a6ffa249f27218aa01f1ebcec97ebce181c
Maguel <miguelangel.1999.10@gmail.com>
2023-12-03 18:21:55 -0600

    Update Empleado.java main and methods

--- tienda-regalos/src/main/java/org/tecnologias/empleado/Empleado.java
+++ tienda-regalos/src/main/java/org/tecnologias/empleado/Empleado.java
@@ -1,150 +1,112 @@
 package org.tecnologias.empleado;
 
+import org.tecnologias.db.DataBase;
 import org.tecnologias.db.Peluche;
-import org.tecnologias.db.Provedor;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Date;
+import java.util.Scanner;
 
-public class Empleado
-{
-    private String nombre;
-    private String apellido;
-    private String contrasena;
-    // Constructor
-    public Empleado(String nombre, String apellido, String contrasena)
-    {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.contrasena = contrasena;
-    }
-    public Empleado()
-    {
+public class Empleado {
+    private static final Scanner sc = new Scanner(System.in);
+    private List<Venta> historial = new ArrayList<>();
+    private static final DataBase db = DataBase.getInstance();
+    private static final Venta venta = new Venta();
 
-    }
-    public void imprimiremplado()
-    {
-        System.out.printf("| %-16s | %-53s |\n", "Nombre: ",nombre);
-        System.out.printf("| %-16s | %-53s |\n","Apellido", apellido );
-
-    }
-    public boolean cancelarProducto(Peluche peluche)
-    {
-        return true;
+    public void cancelarVenta() {
+        venta.limpiar();
     }
 
-    public boolean cancelarVenta(Venta venta)
-    {
-        venta = null;
-        return true;
+    public void historial() {
+        historial.add(venta);
     }
 
-    public boolean eliminarProductoPorId(int idPeluche, Venta ventaActual)
-    {
-        if (ventaActual != null)
-        {
-            List<Peluche> listaPeluches = ventaActual.getListaPeluches();
+    public void cancelarProductoPorId(int idPeluche) {
+        if (!venta.vacia()) {
+            List<Peluche> listaPeluches = new ArrayList<>(venta.getListaPeluches().keySet());
             Iterator<Peluche> iterator = listaPeluches.iterator();// Utiliza un Iterator para poder eliminar elementos durante la iteración
-            while (iterator.hasNext())
-            {
+            while (iterator.hasNext()) {
                 Peluche peluche = iterator.next();
-                if (peluche.getCodigo() == idPeluche)
-                {
+                if (peluche.getCodigo() == idPeluche) {
                     iterator.remove();
                     System.out.println("Peluche eliminado con ID: " + idPeluche);
                 }
             }
-            ventaActual.calcularTotalVenta();
-            return true;
-        }
-        else
-        {
+            venta.calcularTotalVenta();
+        } else {
             System.out.println("No hay venta actual para eliminar el peluche.");
-            return false;
         }
     }
-    public boolean realizarVenta(Peluche peluche,  Venta ventaActual)
-    {
-        if(!ventaActual.vacia())
-        {
-            ventaActual.agregarempleado(this);
-            ventaActual.agregarfecha();
-            ventaActual.ID();
-            //obtenerVentaActual(ventaActual);
-            return true;
+
+    public void realizarVenta() {
+        if (!venta.vacia()) {
+            venta.agregarfecha();
+            venta.ID();
+            historial();
+            venta.imprimirVenta();
+            venta.limpiar();
+            System.out.println("La venta se realizo con exito.");
+            return;
         }
-        else
-        {
-            return false;
-        }
+        System.out.println("La venta no se realizo.");
     }
-    public void agregarProducto(Peluche peluche, Venta ventaActual)
-    {
-        if (ventaActual == null)
-        {
-            ventaActual = new Venta();
-        }
-        ventaActual.agregarpeluche(peluche);
+
+    public void agregarProducto(Peluche peluche) {
+        venta.agregarpeluche(peluche);
     }
-    public void obtenerVentaActual(Venta ventaActual)
-    {
+
+    public void obtenerVentaActual(Venta ventaActual) {
         ventaActual.imprimirVenta();
     }
-    public static void empleadoMain()
-    {
-        Venta venta1 = new Venta();
-        Empleado empleado1 = new Empleado("Juan", "Pérez","123");
-        Empleado empleado2 = new Empleado("carlos", "Pérez","356");
-        //Peluche peluche1 = new Peluche("Juguete", "ABC123", 1, "Marca1", "Osito", 19.99, "Pequeño");
 
-        try (BufferedReader br = new BufferedReader(new FileReader("D:\\5 semestre\\Tecnologias emegergente\\proyecto\\peluches.txt")))
-        {
-            String line;
-            while ((line = br.readLine()) != null)
-            {
-                // Dividir la línea en atributos usando algún delimitador (por ejemplo, ",")
-                String[] attributes = line.split(",");
-
-                // Crear una instancia de Peluche con los atributos y agregarlo a la lista
-            /*    Peluche peluche = new Peluche(
-                        attributes[0].trim(),
-                        attributes[1].trim(),
-                        Integer.parseInt(attributes[2].trim()),
-                        attributes[3].trim(),
-                        attributes[4].trim(),
-                        Double.parseDouble(attributes[5].trim()),
-                        attributes[6].trim()
-                );
-*/
-                // Agregar el peluche a la venta actual del empleado
-               // empleado1.agregarProducto(peluche, venta1);
-               // empleado1.realizarVenta(peluche, venta1);
-                // empleado1.obtenerVentaActual(venta1);
-            }
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-        //  empleado2.agregarProducto(peluche1, venta1);
-        //empleado2.realizarVenta(peluche1, venta1);
-        // Imprimir información de la venta
-        //System.out.println(empleado1.cancelarVenta(venta1));
-        empleado1.eliminarProductoPorId(10, venta1);
-        Peluche peluche3 = new Peluche(1, "osito", Provedor.HASBRO, 7, 50F);
-        empleado1.agregarProducto(peluche3, venta1);
-        empleado1.realizarVenta(peluche3, venta1);
-     /* Peluche peluche4 = new Peluche("Juguete", "ABC123", 12, "Marca1", "Osito", 50, "Pequeño");
-      empleado1.agregarProducto(peluche4, venta1);
-      empleado1.realizarVenta(peluche4, venta1);
-      empleado1.eliminarProductoPorId(10, venta1);*/
-        empleado1.obtenerVentaActual(venta1);
-
+    public static void empleadoMain() {
+        Empleado empleado = new Empleado();
+        String opc, opc1;
+        Peluche peluche;
+        String str;
+        do {
+            System.out.println("Menu Empleado\n1.- Comenzar venta\n2.- Cerrar sesion");
+            opc = sc.nextLine();
+            if (opc.equals("1")) {
+                while (true){
+                    System.out.println("Introduzca el codigo del producto o 'Q' para finalizar la compra");
+                    str = sc.nextLine();
+                    if (str.equalsIgnoreCase("q")) break;
+                    peluche = db.getProducto(Integer.parseInt(str));
+                    if (peluche != null)
+                        empleado.agregarProducto(peluche);
+                    else
+                        System.out.println("El producto no existe");
+                }
+                do {
+                    System.out.println("1.- Finalizar venta\n2.- Cancelar producto\n3.- Cancelar venta");
+                    opc1 = sc.nextLine();
+                    switch (opc1) {
+                        case "1":
+                            empleado.realizarVenta();
+                            break;
+                        case "2":
+                            do {
+                                for (Peluche p: venta.getListaPeluches().keySet()) {
+                                    System.out.print(p.mostar());
+                                }
+                                System.out.println("\nIntroduzca el id del producto a cancelar");
+                                empleado.cancelarProductoPorId(Integer.parseInt(sc.nextLine()));
+                                System.out.println("Desea cancelar otro producto: si/no");
+                            } while (!sc.nextLine().equalsIgnoreCase("no"));
+                            break;
+                        case "3":
+                            empleado.cancelarVenta();
+                            break;
+                        default:
+                            System.out.println("Opcion no valida");
+                    }
+                } while (opc1.equals("2"));
+            } else
+                System.out.println("Cerrando sesion");
+        } while (!opc.equals("2"));
     }
 }
 
