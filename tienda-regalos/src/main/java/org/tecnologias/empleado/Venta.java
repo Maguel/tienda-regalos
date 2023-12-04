b036dcc34a34a816a5f4ea8b3c5c2a59b3245e81
Maguel <miguelangel.1999.10@gmail.com>
2023-12-03 18:23:45 -0600

    Update venta methods

--- tienda-regalos/src/main/java/org/tecnologias/empleado/Venta.java
+++ tienda-regalos/src/main/java/org/tecnologias/empleado/Venta.java
@@ -1,52 +1,39 @@
 package org.tecnologias.empleado;
+
 import org.tecnologias.db.Peluche;
 
-import java.util.Date;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+import java.io.Serializable;
+import java.util.*;
 
-public class Venta
-{
-    private List<Peluche> articulo = new ArrayList<>();
+public class Venta implements Serializable {
+    private Map<Peluche, Integer> articulos = new HashMap<>();
     private Date fecha;
-    // private int id;
-    private Empleado empleado = new Empleado();
-    private static final AtomicLong contador = new AtomicLong(1);
-    private  long id;
+    private Integer id = 1;
+    private Integer cantidad;
 
-    // Constructor
-    public Venta(List<Peluche> articulo, Date fecha, int id, Empleado empleado)
-    {
-        this.articulo = articulo;
-        this.fecha = fecha;
-        this.id = id;
-        this.empleado = empleado;
-    }
-    public Venta()
-    {
+    public void agregarpeluche(Peluche peluche) {
+        if (articulos.containsKey(peluche)) {
+            articulos.put(peluche, ++cantidad);
+        } else {
+            cantidad = 1;
+            articulos.put(peluche, cantidad);
+        }
     }
 
-    public void agregarpeluche(Peluche peluche)
-    {
-        articulo.add(peluche);
-    }
-    public void agregarempleado(Empleado empleado1)
-    {
-        this.empleado = empleado1;
-    }
-    public void agregarfecha()
-    {
-        Date fechaVenta = new Date();
-        this.fecha =fechaVenta;
-    }
-    public void ID()
-    {
-        this.id = contador.getAndIncrement();
 
+    public void agregarfecha() {
+        this.fecha = new Date();
     }
-    public void imprimirVenta()
-    {
+
+    public void ID() {
+        this.id++;
+    }
+
+    public void limpiar() {
+        articulos.clear();
+    }
+
+    public void imprimirVenta() {
         String lineSeparator = "+" + new String(new char[74]).replace('\0', '-') + "+";
         System.out.println(lineSeparator);
         System.out.printf("| %-72s |\n", "Detalles de la Venta");
@@ -55,32 +42,28 @@ public class Venta
         System.out.println(lineSeparator);
         System.out.printf("| %-16s | %-53s |\n", "Fecha de Venta", fecha);
         System.out.println(lineSeparator);
-        for (Peluche peluche : articulo)
-        {
-            peluche.imprimir();
+        for (Peluche peluche : articulos.keySet()) {
+            peluche.imprimir(articulos.get(peluche));
         }
         System.out.println(lineSeparator);
-        empleado.imprimiremplado();
         System.out.printf("| %-16s | %-54s|\n", "Precio total", calcularTotalVenta());
         System.out.println(lineSeparator);
     }
-    public boolean vacia()
-    {
-        return articulo.isEmpty();
+
+    public boolean vacia() {
+        return articulos.isEmpty();
     }
 
-    public double calcularTotalVenta()
-    {
+    public double calcularTotalVenta() {
         double total = 0.0;
-        for (Peluche peluche : articulo)
-        {
-            total += peluche.getPrecio();
+        for (Peluche peluche : articulos.keySet()) {
+            total += peluche.getPrecio() * articulos.get(peluche);
         }
         return total;
     }
-    public List<Peluche> getListaPeluches()
-    {
-        return articulo;
+
+    public Map<Peluche, Integer> getListaPeluches() {
+        return articulos;
     }
 
 }
