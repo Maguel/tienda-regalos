6a988dac38b6d5f02c40d0a5cd602718f3d3599c
Maguel <miguelangel.1999.10@gmail.com>
2023-12-03 18:22:49 -0600

    Update database persistence test

--- tienda-regalos/src/test/java/org/tecnologias/db/DataBaseTests.java
+++ tienda-regalos/src/test/java/org/tecnologias/db/DataBaseTests.java
@@ -3,26 +3,24 @@ package org.tecnologias.db;
 import org.junit.jupiter.api.*;
 
 public class DataBaseTests {
-    @Test
-    public void agregarPelucheTest() {
-        Assertions.assertTrue(DataBase.getInstance().agregarProducto(new Peluche(100326009, "Oso Lotso", Provedor.MATTEL, 10)));
-        Assertions.assertFalse(DataBase.getInstance().agregarProducto(new Peluche(100326009, "Oso Lotso", Provedor.MATTEL, 10)));
-        Assertions.assertTrue(DataBase.getInstance().agregarProducto(new Peluche(100326010, "Teddy", Provedor.MUÑELOCOS, 10)));
-    }
-
-    @Test
-    public void eliminarPeluchesTest() {
-        DataBase db = DataBase.getInstance();
-        Assertions.assertTrue(db.agregarProducto(new Peluche(1003, "Oso Lotso", Provedor.MATTEL, 10)));
-        Assertions.assertFalse(db.agregarProducto(new Peluche(1003, "Oso Lotso", Provedor.MATTEL, 10)));
-        Assertions.assertTrue(db.agregarProducto(new Peluche(1005, "Teddy", Provedor.MUÑELOCOS, 10)));
-        Assertions.assertTrue(db.eliminarProducto(db.getProducto(1005)));
-        Assertions.assertTrue(db.eliminarProducto(db.getProducto(475)));
-    }
-
+    DataBase db = DataBase.getInstance();
+    Peluche.Builder builder = new Peluche.Builder();
     @AfterAll
-    public static void mostrar() {
-        System.out.println("------------------");
+    public static void dbTesting() {
         DataBase.getInstance().getInventario();
+    }
+    @Test
+    public void agregarPeluche() {
+        Assertions.assertTrue(db.agregarProducto(
+                builder.precio(50.56F)
+                        .tamagno("Chiquito")
+                        .clasificacion("Tierno")
+                        .nombre("Peluchito")
+                        .codigo(123123)
+                        .cantidad(60)
+                        .provedor("mattel")
+                        .build()
+                )
+        );
     }
 }
